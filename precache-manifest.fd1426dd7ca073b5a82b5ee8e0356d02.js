b3e5bc4eb00ef08ab2fa413b10b834714ae78c20
ajiir <60216868+ajiir@users.noreply.github.com>
2020-10-12 17:35:52 +0800

    Updates

---
+++ precache-manifest.fd1426dd7ca073b5a82b5ee8e0356d02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78000dda4ffbc4c41241db07605244d",
+    "url": "/%20//ajiir.github.io/Burger/index.html"
+  },
+  {
+    "revision": "e77df5a872d53d4bf045",
+    "url": "/%20//ajiir.github.io/Burger/static/css/main.086a1861.chunk.css"
+  },
+  {
+    "revision": "8ab561e0c0088ce57a19",
+    "url": "/%20//ajiir.github.io/Burger/static/js/2.09be9cde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%20//ajiir.github.io/Burger/static/js/2.09be9cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77df5a872d53d4bf045",
+    "url": "/%20//ajiir.github.io/Burger/static/js/main.9ded48ba.chunk.js"
+  },
+  {
+    "revision": "511b89bf04445cc82a75",
+    "url": "/%20//ajiir.github.io/Burger/static/js/runtime-main.ba2bb554.js"
+  },
+  {
+    "revision": "1ae57197c5383b2372e4e130ac081548",
+    "url": "/%20//ajiir.github.io/Burger/static/media/burger-logo.1ae57197.png"
+  }
+]);
